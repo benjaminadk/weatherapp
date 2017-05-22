846a88244cce511690fead00b8f0b5b5c3456e1b
Benjamin <benjaminadk@gmail.com>
2017-05-22 12:35:24 -0700

    upload weatherapp files from google drive

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/benjaminadk/pen/jmabdb.
+
+ <h1 style="color:white; background-color:black;">Project uses api ip lookup and openweather api to get data on users location and local weather.  App doesnt seem to work with https codepen so if there is  a problem try switching to http.</h1>
---
+++ index.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+  var APIIP = "http://ip-api.com/json";
+  var hour = new Date().getHours();
+
+   var newWindowObj = window.open("", "newWindow","width=500,height=150,top=100,left=100,toolbar=1,location=true,scrollbars=false,menubar=true,resizable=false,personalbar=true");
+  newWindowObj.document.write("<p style='background-color:red; border: 1px solid black;'>hello, if the weather isn't showing up....try deleting the -s- in https in the address bar.  If that doesn't work open me up in debugged and that should do the trick. sorry for the inconvience.</p>");
+  
+  
+  
+  $.getJSON(
+    APIIP,
+    //requesting location data
+    function(data) {
+      var lat = data.lat;
+      var lon = data.lon;
+      var KEY = "6b214ab071f67d626e6b9e926901e3f9";
+      var URL =
+        "http://api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        KEY;
+      //requesting weather data
+
+      $.getJSON(URL, function(data) {
+        var type = data.weather[0].main;
+        var id = data.weather[0].id;
+        var city = data.name;
+        var tempCel = Math.round(data.main.temp - 273.15);
+        var tempC = tempCel + "°";
+        var weather = data.weather[0].description;
+        var tempF = Math.round(tempCel * (9 / 5) + 32) + "°";
+
+        console.log(tempF);
+        console.log(id);
+
+        //setting data on page
+        $(".span1").text(tempF);
+        $(".span2").text(city);
+
+        //want to have one bg for night time
+        /*
+      if(hour > 5 || hour < 18){
+        $('#display').css("background-image", "url ('https://i.kinja-img.com/gawker-media/image/upload/s--EUYvNPnM--/c_scale,f_auto,fl_progressive,q_80,w_800/ke9svxml3ctkvgix59xl.jpg')");
+      }
+      */
+      
+        //if black for different types of weather id
+        
+        //clear sky bg
+        if (id === 800 || id ===801) {
+          $("#display").css({
+            "background-image": "url('http://ak5.picdn.net/shutterstock/videos/1747219/thumb/1.jpg')",
+            "background-position": "-80px 10"
+          });
+        } 
+        
+        //thunderstorm
+        else if (id <= 232 && id >= 200) {
+          $("#display").css({
+            "background-color": "black",
+            "background-image": "url('http://www.clker.com/cliparts/o/9/6/H/6/x/storm-cloud.svg')"
+          });
+        } 
+        
+        //rain bg
+        else if (id < 532 && id >= 500) {
+          $("#display").css({
+            "background-image": "url('http://www.hartstoneinn.com/wp-content/uploads/2013/06/rainy_day.jpg')",
+            "background-position": "-200px 0"
+          });
+        } 
+        
+        //snow bg
+        else if (id <= 622 && id >= 600) {
+          $("#display").css(
+            "background-image",
+            "url('http://uiconstock.com/wp-content/uploads/2014/03/Winter_Background.jpg')"
+          );
+        } 
+        
+        //cloudy no rain bg
+        else if (id <= 804 && id >= 802) {
+          $("#display").css({
+            "background-color": "lightblue",
+            "background-image": "url('http://www.clipartbest.com/cliparts/ncE/BBb/ncEBBbMRi.png')",
+            "background-position": "-120px -190px"
+          });
+        } 
+        
+        //foggy or hazy bg
+        else if (id < 782 && id > 700) {
+          $("#display").css({"background-image":"url('http://clipground.com/images/fog-day-clipart-9.jpg')", "background-size": "cover"});
+        } 
+
+        //default chicken cartoon bg        
+        
+        else {
+          $("#display").css({
+            "background-image": "url('https://www.wpclipart.com/cartoon/animals/bird/bird_cartoons_2/confused_chickens.png')",
+            "background-position": "140px 50px"
+          });
+        }
+
+        //celcius farenheit toggle
+        $(".node2").on("click", function() {
+          $("#cel, #far").toggleClass("invisible");
+          if ($(".span1").text() === tempF) {
+            $(".span1").text(tempC);
+          } else {
+            $(".span1").html(tempF);
+          }
+        });
+      });
+    }
+  );
+});
